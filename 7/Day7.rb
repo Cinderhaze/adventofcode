d220bbfe4d7e1674ffc91c4af57fb784afecc12d
Daryl Wiest <daryl.wiest@gmail.com>
2015-12-11 00:13:06 -0500

    Started on day 7, implemented the binary operations

---
+++ 7/Day7.rb
@@ -0,0 +1,95 @@
+#!/usr/bin/env ruby
+
+require 'getoptlong'
+
+class Inst
+  attr_reader :cmd, :result, :operands
+
+  def initialize(inst)
+    @operands= Array.new
+    parseInst(inst)
+  end
+
+  def parseInst(inst)
+    #use a regex to create three matchgrups
+    case inst
+    when /(?<cmd>.*)\s(?<start>\d*,\d*) through (?<stop>\d*,\d*)/ =~ inst
+    when /(.*) (OR|AND|[L|R]SHIFT) (.*) -> (.*)/
+      @operands << $1
+      @operands << $3
+      @cmd = $2
+      @result = $4
+    end
+  end
+end
+
+class Circuit
+  attr_reader :input, :results
+  
+  def initialize()
+    @results = Hash.new
+    @steps = Array.new
+    
+  end
+
+  def step(instruction)
+    steps << Inst.new(instruction)
+  end
+
+  def calc()
+    #Take each 'result' value from the instructions and iterativly/recursivly look up their values
+  end
+
+end
+
+class Day7
+  attr_reader :input
+  
+  def initialize(input)
+    @input = input
+  end
+
+  def calc()
+
+    circuit = Circuit.new()
+    @input.each_line do |line|
+      circuit.step(line)
+    end
+
+    circuit.calc()
+
+    puts circuit.results
+
+  end
+ 
+end
+
+#Actually execute things here.. figure out the best way to do this
+if __FILE__ == $0
+
+  opts = GetoptLong.new(
+    [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+    [ '--file', '-f', GetoptLong::OPTIONAL_ARGUMENT ],
+    [ '--input', '-i', GetoptLong::OPTIONAL_ARGUMENT ],
+  )
+
+  input_name = 'day7.input.txt'
+  input = nil
+  
+  opts.each do |opt, arg|
+    case opt
+      when '--file'
+        input_name = arg.to_s
+      when '--input'
+        input = arg.to_s
+    end
+  end
+
+  if !input 
+    input = IO.read(input_name)
+  end
+  
+  ans = Day7.new(input)
+  ans.calc()
+
+end
---
+++ 7/test.rb
@@ -0,0 +1,47 @@
+load 'Day7.rb'
+
+describe Inst do
+#  it "means that the signal 123 is provided to wire x" do
+#    inst = Inst.new('123 -> x')
+#    expect(inst.result).to eq('x')
+#    expect(inst.cmd).to eq('SIG')
+#    expect(inst.operands).to eq(['123'])
+#  end
+  it "means that the bitwise AND of wire x and wire y" do
+    inst = Inst.new('x AND y -> z')
+    expect(inst.result).to eq('z')
+    expect(inst.cmd).to eq('AND')
+    expect(inst.operands).to eq(['x','y'])
+  end
+  it "means that the bitwise OR of wire x and wire y" do
+    inst = Inst.new('x OR y -> z')
+    expect(inst.result).to eq('z')
+    expect(inst.cmd).to eq('OR')
+    expect(inst.operands).to eq(['x','y'])
+  end
+  it "means that the value from wire p is left-shifted by 2 and then provided to wire q" do
+    inst = Inst.new('p LSHIFT 2 -> q')
+    expect(inst.result).to eq('q')
+    expect(inst.cmd).to eq('LSHIFT')
+    expect(inst.operands).to eq(['p','2'])
+  end
+  it "means that the value from wire p is right-shifted by 2 and then provided to wire q" do
+    inst = Inst.new('p RSHIFT 2 -> q')
+    expect(inst.result).to eq('q')
+    expect(inst.cmd).to eq('RSHIFT')
+    expect(inst.operands).to eq(['p','2'])
+  end
+#  it "means that the bitwise complement of the value from wire e is provided to wire f" do
+#    inst = Inst.new('NOT e -> f')
+#    expect(inst.result).to eq('f')
+#    expect(inst.cmd).to eq('NOT')
+#    expect(inst.operands).to eq(['e'])
+#  end
+end
+
+describe Circuit do
+#  it "should initialize to having no lights lit" do
+#    grid = Grid.new()
+#    expect(grid.num_on()).to equal(0)
+#  end
+end
