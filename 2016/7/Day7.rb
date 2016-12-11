c8e5bff7a4cd5fb5bafdc30abd392c85e3f5074f
Daryl Wiest <daryl.wiest@gmail.com>
2016-12-11 01:54:28 -0500

    Example passing, but I was only accounting for pre[hyper]post format

--- 2016/7/Day7.rb
+++ 2016/7/Day7.rb
@@ -11,14 +11,38 @@ class Ipv7
 
   # Returns an array of 4 letter palindromes in the given string
   def abba(str)
-    str.chars.each_cons(4).select{|word| word == word.reverse}.map{|ary| ary.join}
+    str.chars.each_cons(4)
+      .select{|word| word == word.reverse and word[0] != word[1]}
+      .map{|ary| ary.join}
   end
 
   def supportsTls
+    # does not have abba within hypernet
+    # AND
     # has abba outside of hypernet
-    p abba(@pre)
-    p abba(@post)
-    # also does not have abba within hypernet
+    abba(@hypernet).empty? and (! abba(@pre).empty? or ! abba(@post).empty?)
   end
 
+end
+
+class Day7
+  attr_reader :input
+  def initialize(input)
+    @input = input
+  end
+
+  def count_tls_supporters
+    supports = 0
+    @input.each_line do |line|
+      ipv7 = Ipv7.new(line)
+      supports += 1 if ipv7.supportsTls
+    end
+    supports
+  end
+
+  def supports
+    count_tls_supporters
+  end
+
+  
 end
--- 2016/7/test_day7.rb
+++ 2016/7/test_day7.rb
@@ -3,6 +3,7 @@ require 'minitest/autorun'
 require_relative './Day7.rb'
 
 class TestNumberpad < Minitest::Test
+  #abba[mnop]qrst supports TLS (abba outside square brackets).
   def test_ABBA_before_brackets_supports_TLS
       input = 'abba[mnop]qrst'
       ipv7 = Ipv7.new(input)
@@ -19,8 +20,39 @@ class TestNumberpad < Minitest::Test
      
   end
 
-  #abba[mnop]qrst supports TLS (abba outside square brackets).
   #abcd[bddb]xyyx does not support TLS (bddb is within square brackets, even though xyyx is outside square brackets).
+  def test_ABBA_within_brackets_does_not_supports_TLS
+      input = 'abcd[bddb]xyyx'
+      ipv7 = Ipv7.new(input)
+      actual_value = ipv7.supportsTls
+      assert( !actual_value)
+  end
   #aaaa[qwer]tyui does not support TLS (aaaa is invalid; the interior characters must be different).
+  def test_ABBA_interior_chars_must_be_diff_not_supports_TLS
+      input = 'aaaa[qwer]tyui'
+      ipv7 = Ipv7.new(input)
+      actual_value = ipv7.supportsTls
+      assert(! actual_value)
+  end
   #ioxxoj[asdfgh]zxcvbn supports TLS (oxxo is outside square brackets, even though it's within a larger string).
+  def test_ABBA_within_larger_string_supports_TLS
+      input = 'ioxxoj[asdfgh]zxcvbn'
+      ipv7 = Ipv7.new(input)
+      actual_value = ipv7.supportsTls
+      assert(actual_value)
+  end
+
+  def test_count_ips_that_support_tls
+    #1&4 support, 2&3 do not
+    input = <<-EOF
+abba[mnop]qrst
+abcd[bddb]xyyx
+aaaa[qwer]tyui
+ioxxoj[asdfgh]zxcvbn
+    EOF
+    day7 = Day7.new(input)
+    actual = day7.supports
+    assert_equal(2, actual)
+
+  end
 end
